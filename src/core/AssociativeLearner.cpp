69ecdb77a43f21918cf4976cf19a12f17770d91b
Dominik <dominikmaslak11@gmail.com>
2026-05-04 17:44:44 +0200

    Krok 18: Wielowymiarowa macierz korelacji zmiennych (heatmapa)

--- src/core/AssociativeLearner.cpp
+++ src/core/AssociativeLearner.cpp
@@ -152,6 +152,22 @@ AssociativeLearner::AssociativeLearner(QWidget *parent) : QWidget(parent) {
     m_markovTable->setMinimumHeight(200);
     mainLayout->addWidget(m_markovTable);
     m_markovTimer = new QTimer(this);
+    // --- Macierz korelacji zmiennych ---
+    auto *crossVarLayout = new QHBoxLayout;
+    crossVarLayout->addWidget(new QLabel("Macierz korelacji zmiennych:"));
+    m_crossVarBtn = new QPushButton("Pokaż macierz korelacji zmiennych");
+    crossVarLayout->addWidget(m_crossVarBtn);
+    crossVarLayout->addStretch();
+    mainLayout->addLayout(crossVarLayout);
+    m_crossVarTable = new QTableWidget(0,0);
+    m_crossVarTable->verticalHeader()->hide();
+    m_crossVarTable->horizontalHeader()->setStretchLastSection(true);
+    m_crossVarTable->setShowGrid(false);
+    m_crossVarTable->setAlternatingRowColors(false);
+    m_crossVarTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    m_crossVarTable->setMinimumHeight(300);
+    mainLayout->addWidget(m_crossVarTable);
+    connect(m_crossVarBtn, &QPushButton::clicked, this, &AssociativeLearner::updateCrossVariableMatrix);
     connect(m_markovTimer, &QTimer::timeout, this, &AssociativeLearner::predictNextFrames);
     connect(m_trainMarkovBtn, &QPushButton::clicked, this, [this]() {
         trainMarkovModel();
@@ -701,5 +717,68 @@ void AssociativeLearner::predictNextFrames() {
         m_markovTable->item(i, 0)->setForeground(QColor("#c0c0c0"));
         m_markovTable->item(i, 1)->setForeground(col);
         m_markovTable->item(i, 2)->setForeground(col);
+    }
+}
+
+// ---------- Macierz korelacji zmiennych ----------
+void AssociativeLearner::updateCrossVariableMatrix() {
+    QStringList varNames = m_observationsMap.keys();
+    int N = varNames.size();
+    if (N < 2) {
+        m_crossVarTable->setRowCount(0);
+        m_crossVarTable->setColumnCount(0);
+        return;
+    }
+
+    // Zbuduj wektory wartości dla każdej zmiennej (uśrednione w oknach czasowych)
+    QVector<QVector<double>> series(N);
+    for (int i = 0; i < N; ++i) {
+        const QVector<ValueObservation> &obs = m_observationsMap[varNames[i]];
+        for (const auto &o : obs)
+            series[i].append(o.value);
+    }
+
+    // Oblicz macierz korelacji Pearsona
+    m_crossVarTable->setRowCount(N);
+    m_crossVarTable->setColumnCount(N);
+    m_crossVarTable->setHorizontalHeaderLabels(varNames);
+    m_crossVarTable->setVerticalHeaderLabels(varNames);
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            const QVector<double> &X = series[i];
+            const QVector<double> &Y = series[j];
+            int len = qMin(X.size(), Y.size());
+            if (len < 3) {
+                m_crossVarTable->setItem(i, j, new QTableWidgetItem("N/A"));
+                continue;
+            }
+
+            double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0, sumY2 = 0;
+            for (int k = 0; k < len; ++k) {
+                double x = X[k], y = Y[k];
+                sumX += x; sumY += y;
+                sumXY += x * y;
+                sumX2 += x * x;
+                sumY2 += y * y;
+            }
+            double denom = sqrt((len * sumX2 - sumX * sumX) * (len * sumY2 - sumY * sumY));
+            double corr = (denom != 0) ? (len * sumXY - sumX * sumY) / denom : 0.0;
+
+            QTableWidgetItem *item = new QTableWidgetItem(QString::number(corr, 'f', 2));
+            // Kolor: od niebieskiego (-1) przez żółty (0) do zielonego (+1)
+            int r, g, b;
+            if (corr < 0) {
+                r = (int)(255 * (1 + corr));
+                g = (int)(255 * (1 + corr));
+                b = 255;
+            } else {
+                r = (int)(255 * (1 - corr));
+                g = 255;
+                b = (int)(255 * (1 - corr));
+            }
+            item->setBackground(QColor(r, g, b));
+            m_crossVarTable->setItem(i, j, item);
+        }
     }
 }
